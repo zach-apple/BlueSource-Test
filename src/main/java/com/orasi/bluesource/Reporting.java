72e3ef9300fa62242ed6137b5a1501137b377510
daniel.smith <daniel.smith@GSOD-982017.orasi.com>
2017-11-09 11:23:17 -0500

    reporting_accountReports_billingByEmployee addition

--- src/main/java/com/orasi/bluesource/Reporting.java
+++ src/main/java/com/orasi/bluesource/Reporting.java
@@ -1,6 +1,20 @@
 package com.orasi.bluesource;
 
+import java.awt.List;
+import java.util.Date;
+import java.util.Random;
+
+import org.openqa.selenium.support.FindBy;
+
+import com.orasi.utils.TestReporter;
 import com.orasi.web.OrasiDriver;
+import com.orasi.web.webelements.Button;
+import com.orasi.web.webelements.Element;
+import com.orasi.web.webelements.Label;
+import com.orasi.web.webelements.Link;
+import com.orasi.web.webelements.Listbox;
+import com.orasi.web.webelements.Textbox;
+import com.orasi.web.webelements.Webtable;
 import com.orasi.web.webelements.impl.internal.ElementFactory;
 
 public class Reporting {
@@ -9,11 +23,128 @@ public class Reporting {
 	
 	/**Page Elements**/
 	
+	/*Login error message from page*/
+	@FindBy(xpath = "//div[@class = 'alert alert-danger alert-dismissable']") private Label lblPageAlert;
+	
+	/**Reporting drop downs**/
+	@FindBy(linkText = "Account Reports") private Link lnkAccountReports;
+	@FindBy(linkText = "Employee Reports") private Link lnkEmployeeReports;
+	@FindBy(linkText = "Department Reports") private Link lnkDepartmentReports;
+	@FindBy(linkText = "Project Reports") private Link lnkProjectReports;
+	@FindBy(linkText = "Admin Reports") private Link lnkAdminReports;
+	
+	/*Links under account reporting*/
+	@FindBy(linkText = "Time by Employee") private Link lnkTimeByEmployee;
+	@FindBy(linkText = "Time by Time Sheet") private Link lnkTimeByTimeSheet;
+	@FindBy(linkText = "Billing by Employee") private Link lnkBillingByEmployee;
+	@FindBy(linkText = "Burn Down") private Link lnkBurnDown;
+	@FindBy(linkText = "Burn down Data") private Link lnkBurnDownData;
+	
+	/*Modal elements*/
+	@FindBy(xpath = "//div[@class = 'modal-content']") private Element elePopover;
+	@FindBy(xpath = "//label[contains(text(), 'Summary')]") private Label lblSummary;
+	@FindBy(xpath = "//label[contains(text(), 'Detail')]") private Label lblDetail;
+	@FindBy(xpath = "//div[contains(text(), 'Select All')]") private Link lnkSelectAll;
+	@FindBy(id = "start_date") private Textbox txtStartDate;
+	@FindBy(id = "end_date") private Textbox txtEndDate;
+	@FindBy(xpath = "//input[contains(@value, 'Generate Report')]") private Button btnGenReport;
+	
 	
 	public Reporting(OrasiDriver driver)
 	{
 		this.driver = driver;
 		ElementFactory.initElements(driver, this);
+	}
+	
+	/*
+	 * Click on the Account Reports drop down
+	 * -- Sub links should be present after the click
+	 * @author: Daniel Smith
+	 */
+	public void click_accountReports()
+	{
+		if (lnkAccountReports.isDisplayed() == true)
+		{
+			lnkAccountReports.syncVisible();
+			lnkAccountReports.click();
+		}
+	}
+	
+	/*
+	 * Failed login error message from page
+	 * @author: Daniel Smith
+	 */
+	public void check_for_error(String username)
+	{
+		Boolean pageError = lblPageAlert.isDisplayed();
+		
+		if(pageError == true)
+		{
+			System.out.println("Error stating: " + lblPageAlert.getText() + " for " + username);
+		}
+	}
+	
+	/*
+	 * Click on billing by employee 
+	 * @author: Daniel Smith
+	 */
+	public void click_billingByEmployee()
+	{
+		if(lnkBillingByEmployee.isDisplayed() == true)
+		{
+			lnkBillingByEmployee.syncVisible();
+			lnkBillingByEmployee.click();
+		}
+	}
+	
+	/*
+	 * Check that the modal is displaying 
+	 * @author: Daniel smith
+	 */
+	public void check_modal()
+	{
+		Boolean visible = elePopover.isDisplayed();
+		
+		if(visible = true)
+		{
+			elePopover.syncVisible();
+			TestReporter.assertTrue(visible, "Modal is visible");
+		}
+		
+	}
+	
+	/*
+	 * Fill out Account Billing by Employee Report modal form 
+	 * @author: Daniel Smith
+	 */
+	public void fill_out_billing_by_employee(String startDate, String endDate)
+	{
+		//random number generate to choose between summary and detail
+		Random number;
+		number = new Random();
+		int ranNumber = number.nextInt(2);
+		
+		if(ranNumber == 0)
+		{
+			lblSummary.click();
+			System.out.println("Summary radio button has been selected");
+		}
+		else if (ranNumber == 1)
+		{
+			lblDetail.click();
+			System.out.println("Detail radio button has been selected");
+		}
+		System.out.println("Random number " + ranNumber);
+		
+		//select all accounts
+		lnkSelectAll.click();
+		
+		//select a start and end date
+		txtStartDate.set(startDate);
+		txtEndDate.set(endDate);
+		
+		//Generate report
+		btnGenReport.click();
 	}
 	
 }
--- src/test/java/com/bluesource/reporting/Reporting_AccountReports_BillingByEmployee.java
+++ src/test/java/com/bluesource/reporting/Reporting_AccountReports_BillingByEmployee.java
@@ -1,5 +1,7 @@
 package com.bluesource.reporting;
 
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriverException;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -12,6 +14,7 @@ import com.orasi.bluesource.Accounts;
 import com.orasi.bluesource.LoginPage;
 import com.orasi.bluesource.MessageCenter;
 import com.orasi.bluesource.Reporting;
+import com.orasi.utils.TestReporter;
 import com.orasi.utils.dataProviders.ExcelDataProvider;
 import com.orasi.web.OrasiDriver;
 import com.orasi.web.WebBaseTest;
@@ -25,7 +28,7 @@ public class Reporting_AccountReports_BillingByEmployee extends WebBaseTest{
 		// **************
 		@DataProvider(name = "reporting_billingByEmployee", parallel=true)
 		public Object[][] scenarios() {
-			return new ExcelDataProvider("/excelsheets/blueSource_Users.xlsx", "Sheet1").getTestData();
+			return new ExcelDataProvider("/excelsheets/blueSource_reporting_withDates.xlsx", "Sheet1").getTestData();
 		}
 					
 		@BeforeMethod
@@ -52,24 +55,63 @@ public class Reporting_AccountReports_BillingByEmployee extends WebBaseTest{
 		 * Test steps:
 		 * Open browser / navigate to http://10.238.243.127:8080/reporting/login
 		 * Login into the page
-		 * select the billing by employee tab
+		 * select the billing by employee tab under account reports
 		 * select summary radio button
-		 * select the account name for account dropdown
+		 * select the account name for account drop down
 		 * select a start and end date
 		 * select a project from the list
 		 * logout
 		 */
 
 		@Test(dataProvider = "reporting_billingByEmployee")
-		public void reporting_billingByEmployee_test(String username, String password)
+		public void reporting_billingByEmployee_test(String username, String password, String startDate, String endDate)
 		{
+			//declare local variables
+			String currentPage, expectedPage, message;
+			
 			//declare page objects
 			LoginPage loginpage = new LoginPage(getDriver());
 	    	Reporting reportingPage = new Reporting(getDriver());
 	    	MessageCenter messageCenterpage = new MessageCenter(getDriver());
 	    	
-	    	loginpage.LoginWithCredentials(username, password);
-	    
+	    	//Initialize variables 
+	    	currentPage = getDriver().getCurrentUrl();
+	    	expectedPage = "http://10.238.243.127:8080/reporting/login";
+	    	message = "On reporting page";
 	    	
+	    	//check that the current page is the reporting page
+	    	TestReporter.assertEquals(currentPage, expectedPage, message);
+	    	
+	    	try
+	    	{
+	    		//login into the reporting page
+		    	loginpage.LoginWithCredentials(username, password);
+		    	
+		    	//click on the account reports drop down
+		    	reportingPage.click_accountReports();
+		    	
+		    	//click on billing by employee under account reports
+		    	reportingPage.click_billingByEmployee();
+		    	
+		    	//check that the modal appears
+		    	reportingPage.check_modal();
+		    	
+		    	//fill out modal information
+		    	reportingPage.fill_out_billing_by_employee(startDate, endDate);
+		    	
+	    	}
+	    	catch(WebDriverException e)
+	    	{
+	    		try
+	    		{
+	    			//check for page error upon login
+			    	reportingPage.check_for_error(username);
+			    	TestReporter.log(e.getLocalizedMessage());
+	    		}catch(NoSuchElementException err)
+	    		{
+	    			TestReporter.log(err.getLocalizedMessage());
+	    		}
+	    	}
+	    
 		}
 }
